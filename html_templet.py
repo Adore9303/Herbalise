2c65145103554a1feee217c07901c66052ea526f
Abhishek M <107853973+Adore9303@users.noreply.github.com>
2023-09-22 14:55:31 +0530

    Add files via upload

---
+++ html_templet.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://img.freepik.com/premium-vector/robot-icon-circle-vector-illustration_418020-199.jpg?w=740">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://images.freeimages.com/fic/images/icons/766/base_software/256/user1.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
